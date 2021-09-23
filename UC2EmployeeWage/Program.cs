6e4a429751216cede10ed25681d693e08310440a
HemanthGK-GK <hemanthgk804@gmail.com>
2021-09-23 12:33:37 +0530

    Hemanth : Calculating EmployeeWage for a Day

---
+++ UC2EmployeeWage/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace UC2EmployeeWage
+{
+    class EmpWage
+    {
+        static void Main(string[] args)
+        {
+            int fullTime = 1;
+            int empRate = 20;
+            int empWage = 0;
+            int hours = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == fullTime)
+                hours = 8;
+            else
+                hours = 0;
+
+            empWage = empRate * hours;
+            Console.WriteLine("Employee Wage :" + empWage);
+
+        }
+    }
+
+}

0a44b51412dbd2e9763f15ef198bb4922acc951a
HemanthGK-GK <hemanthgk804@gmail.com>
2021-09-23 12:38:37 +0530

    Hemanth : Calculating EmployeWages for PartTime

---
+++ UC3EmpWagePartTime/EmpPartTime.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UC3EmpWagePartTime
+{
+    class EmpPartTime
+    {
+        static void Main(string[] args)
+        {
+            int fullTime = 2;
+            int partTime = 1;
+            int empRate = 20;
+
+            int empWage = 0;
+            int hours = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+            if (empCheck == fullTime)
+                hours = 12;
+            else if (empCheck == partTime)
+                hours = 8;
+            else
+                hours = 0;
+            empWage = empRate * hours;
+            Console.WriteLine("Employee Wage :" + empWage);
+
+        }
+    }
+}

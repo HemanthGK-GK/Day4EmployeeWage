8976f865143344638b19c98a21345d90a021d875
HemanthGK-GK <hemanthgk804@gmail.com>
2021-09-23 12:44:49 +0530

    Hemanth : Calculating EmployeWages for a Month

---
+++ UC5WagesForMonth/EmpWagesForMonth.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace UC5WagesForMonth
+{
+    class EmpWagesForMonth
+    {
+        static void Main(string[] args)
+        {
+            const int fullTime = 2;
+            const int partTime = 1;
+            const int empRate = 20;
+            const int totalDays = 20;
+            int totalWage = 0;
+            int empWage = 0;
+            int hours = 0;
+
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+
+            for (int day = 0; day < totalDays; day++)
+            {
+                switch (empCheck)
+                {
+                    case fullTime:
+                        hours = 12;
+                        break;
+                    case partTime:
+                        hours = 8;
+                        break;
+                    default:
+                        hours = 0;
+                        break;
+                }
+
+            }
+            empWage = hours * empRate;
+            totalWage += empWage;
+
+            Console.WriteLine("Employee Wage new:" + empWage);
+
+        }
+    }
+
+}
